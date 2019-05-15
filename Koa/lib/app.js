5513c864595db73de0a6ce874bd6f7fe968bc704
hbxywdk <364746078@qq.com>
2019-05-15 18:25:08 +0800

    koa

---
+++ Koa/lib/app.js
@@ -0,0 +1,63 @@
+const Koa = require('./application.js')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  console.log(1)
+  next()
+})
+app.use(async (ctx, next) => {
+  console.log(2)
+  next()
+})
+app.use(async ctx => {
+  console.log(3)
+  ctx.body = 'Hello World'
+})
+
+app.listen(3333)
+
+
+var middleware = [() => console.log(1), () => console.log(2), () => console.log(3)] // 中间件
+var index = 0 // 起始下标
+
+function run() {
+  var fn = middleware[index] // 当前要执行的中间件
+  index++
+  // 没有中间件了，返回一个没有参数的 Promise.resolve()
+  if (!fn) return Promise.resolve()
+  return Promise.resolve(run(fn()))
+}
+
+run().then(function () {
+  console.log('End')
+})
+
+
+// 中间件
+var middleware = [
+  (ctx, next) => { console.log(1); next() },
+  (ctx, next) => { console.log(2); next() },
+  (ctx, next) => { console.log(3) }
+]
+var ctx = {} // 模拟 ctx 这里直接给个空对象
+var index = 0 // 起始下标
+
+function run(ctx) {
+  return dispatch(0)
+  function dispatch(i) {
+    index = i
+    var fn = middleware[i] // 当前要执行的中间件
+    // 没有中间件了，返回一个没有参数的 Promise.resolve()
+    if (!fn) return Promise.resolve()
+    // dispatch.bind(null, i + 1) = 中间件的 next 参数 = 下一个中间件
+    return Promise.resolve( fn(ctx, dispatch.bind(null, i + 1)) )
+  }
+}
+
+run(ctx)
+.then(() => {
+  console.log('res.end()')
+})
+.catch(err => {
+  console.error(err)
+})
--- Koa/lib/application.js
+++ Koa/lib/application.js
@@ -28,7 +28,6 @@ const context = require('./context');
 module.exports = class Application extends Emitter {
   /**
    * 初始化一个新的 `Application`.
-   *
    * @api public
    */
   constructor() {
@@ -108,7 +107,9 @@ module.exports = class Application extends Emitter {
    * @api public
    */
   callback() {
+    // compose会返回一个函数，执行这个函数则会依次使用 Promise.resolve 来执行各个中间件
     const fn = compose(this.middleware);
+    console.log(fn.toString())
     if (!this.listenerCount('error')) this.on('error', this.onerror);
     // 定义handleRequest
     const handleRequest = (req, res) => {
@@ -126,28 +127,34 @@ module.exports = class Application extends Emitter {
   handleRequest(ctx, fnMiddleware) {
     const res = ctx.res;
     res.statusCode = 404;
+    // 错误处理
     const onerror = err => ctx.onerror(err);
+    // 处理 response
     const handleResponse = () => respond(ctx);
+    // 当HTTP请求关闭、完成或发生错误时执行 onerror 回调
     onFinished(res, onerror);
+    // 使用 Promise.resolve() 依次执行中间件，所有中间件执行完成，则会调用 respond(ctx) 自动帮我们 res.end()
     return fnMiddleware(ctx).then(handleResponse).catch(onerror);
   }
   /**
    * 初始化一个新 context.
    * @api private
    */
-  createContext(req, res) {
+  createContext(req, res) { // 参数 req、res 是 http.createServer 中得到的 req、res
+    // context 是一个Object，request与response会挂载在它上面
     const context = Object.create(this.context);
     const request = context.request = Object.create(this.request);
     const response = context.response = Object.create(this.response);
-    context.app = request.app = response.app = this;
-    context.req = request.req = response.req = req;
-    context.res = request.res = response.res = res;
-    request.ctx = response.ctx = context;
+
+    context.app = request.app = response.app = this; // 将它们的 app 属性全部指向 this
+    context.req = request.req = response.req = req; // 挂载原始的 req 在各自 req 属性上
+    context.res = request.res = response.res = res; // 挂载原始的 res 在各自 res 属性上
+    request.ctx = response.ctx = context; // 挂载 response 到 request、response 的 ctx 属性上
     request.response = response;
     response.request = request;
-    context.originalUrl = request.originalUrl = req.url;
-    context.state = {};
-    return context;
+    context.originalUrl = request.originalUrl = req.url; // 挂载请求的 url
+    context.state = {}; // state
+    return context; // 返回
   }
   /**
    * 默认的错误处理
@@ -169,29 +176,25 @@ module.exports = class Application extends Emitter {
 
 // Response helper.
 function respond(ctx) {
+  console.log('=== 执行respond ===')
   // allow bypassing koa
   if (false === ctx.respond) return;
-
   if (!ctx.writable) return;
-
   const res = ctx.res;
   let body = ctx.body;
   const code = ctx.status;
-
   // ignore body
-  if (statuses.empty[code]) {
+  if (statuses.empty[code]) { // 状态码为空
     // strip headers
     ctx.body = null;
     return res.end();
   }
-
   if ('HEAD' == ctx.method) {
     if (!res.headersSent && isJSON(body)) {
       ctx.length = Buffer.byteLength(JSON.stringify(body));
     }
     return res.end();
   }
-
   // status body
   if (null == body) {
     if (ctx.req.httpVersionMajor >= 2) {
@@ -205,7 +208,6 @@ function respond(ctx) {
     }
     return res.end(body);
   }
-
   // responses
   if (Buffer.isBuffer(body)) return res.end(body);
   if ('string' == typeof body) return res.end(body);
