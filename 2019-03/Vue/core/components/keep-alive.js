28049f37c9773f04628f39d87f413cb276328be4
dirk.wang <dirk.wang@huolala.cn>
2019-06-26 17:57:08 +0800

    keep-alive

--- 2019-03/Vue/core/components/keep-alive.js
+++ 2019-03/Vue/core/components/keep-alive.js
@@ -5,10 +5,12 @@ import { getFirstComponentChild } from 'core/vdom/helpers/index'
 
 type VNodeCache = { [key: string]: ?VNode };
 
+// 获取组件名称或tag标签
 function getComponentName (opts: ?VNodeComponentOptions): ?string {
   return opts && (opts.Ctor.options.name || opts.tag)
 }
 
+// 匹配 pattern 中是否有 name，有返回 true，无返回 false
 function matches (pattern: string | RegExp | Array<string>, name: string): boolean {
   if (Array.isArray(pattern)) {
     return pattern.indexOf(name) > -1
@@ -21,8 +23,18 @@ function matches (pattern: string | RegExp | Array<string>, name: string): boole
   return false
 }
 
+/**
+ * 
+ * @param {any} keepAliveInstance keep-alive实例
+ * @param {Function} filter 返回是否匹配上的函数
+ */
 function pruneCache (keepAliveInstance: any, filter: Function) {
+  // cache（create中创建的一个空对象）
+  // keys（create中创建的一个空数组）
+  // _vnode（keep-alive组件的_vnode属性）
   const { cache, keys, _vnode } = keepAliveInstance
+
+  // 遍历 cache
   for (const key in cache) {
     const cachedNode: ?VNode = cache[key]
     if (cachedNode) {
@@ -32,8 +44,10 @@ function pruneCache (keepAliveInstance: any, filter: Function) {
       }
     }
   }
+
 }
 
+// 销毁实例，并从 cache 中移除
 function pruneCacheEntry (
   cache: VNodeCache,
   key: string,
@@ -50,44 +64,53 @@ function pruneCacheEntry (
 
 const patternTypes: Array<Function> = [String, RegExp, Array]
 
+// keep-alive组件 周期：created -> render -> mounted
 export default {
   name: 'keep-alive',
   abstract: true,
 
   props: {
-    include: patternTypes,
-    exclude: patternTypes,
-    max: [String, Number]
+    include: patternTypes, // 字符串或正则表达式。只有名称匹配的组件会被缓存。
+    exclude: patternTypes, // 字符串或正则表达式。任何名称匹配的组件都不会被缓存。
+    max: [String, Number] // 数字。最多可以缓存多少组件实例。
   },
-
+  
+  // created
   created () {
     this.cache = Object.create(null)
     this.keys = []
   },
 
+  // mounted
+  mounted () {
+    // 监听 include，变化后执行 pruneCache
+    this.$watch('include', val => {
+      pruneCache(this, name => matches(val, name))
+    })
+    // 监听 exclude，变化后执行 pruneCache
+    this.$watch('exclude', val => {
+      pruneCache(this, name => !matches(val, name))
+    })
+  },
+
+  // destroyed 时销毁虽有组件
   destroyed () {
     for (const key in this.cache) {
       pruneCacheEntry(this.cache, key, this.keys)
     }
   },
 
-  mounted () {
-    this.$watch('include', val => {
-      pruneCache(this, name => matches(val, name))
-    })
-    this.$watch('exclude', val => {
-      pruneCache(this, name => !matches(val, name))
-    })
-  },
-
   render () {
-    const slot = this.$slots.default
-    const vnode: VNode = getFirstComponentChild(slot)
-    const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
+    const slot = this.$slots.default // 获取 slot
+    // 获取 slot 中第一个有效 component
+    const vnode: VNode = getFirstComponentChild(slot) 
+    const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions // vnode 的配置项
     if (componentOptions) {
       // check pattern
+       // 组件名，没有组件名就返回 tag 名
       const name: ?string = getComponentName(componentOptions)
       const { include, exclude } = this
+      // 不在 included 或者说 在 excluded 中，则是不缓存的组件，直接返回 vnode。
       if (
         // not included
         (include && (!name || !matches(include, name))) ||
@@ -98,25 +121,30 @@ export default {
       }
 
       const { cache, keys } = this
+      // vnode 有 key 则赋值为 key，没 key 则赋一个 key，这个 key 用于缓存组件
       const key: ?string = vnode.key == null
         // same constructor may get registered as different local components
         // so cid alone is not enough (#3269)
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
+
+      // 已缓存过，直接取缓存中的 componentInstance 给 vnode
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
         remove(keys, key)
         keys.push(key)
+      // 未缓存过，添加缓存
       } else {
         cache[key] = vnode
         keys.push(key)
         // prune oldest entry
+        // 最大缓存组件
         if (this.max && keys.length > parseInt(this.max)) {
           pruneCacheEntry(cache, keys[0], keys, this._vnode)
         }
       }
-
+      // keepAlive 标记
       vnode.data.keepAlive = true
     }
     return vnode || (slot && slot[0])
